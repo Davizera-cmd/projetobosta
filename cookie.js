17022275177fcc80f4e65fd8dd5e4d201cd303c8
Davi Amorim de Lima <davi.lima@aluno.fmpsc.edu.br>
2024-10-14 14:53:40 -0300

    Add files via upload

---
+++ cookie.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cookieBox = document.querySelector(".cookieWrapper"),
+          acceptBtn = cookieBox.querySelector(".cookieItem");
+
+    // Verifica se o cookie já foi aceito
+    let checkCookie = document.cookie.indexOf("CookieAccepted=true");
+
+    if (checkCookie !== -1) {
+        cookieBox.classList.add("hide"); // Oculta o pop-up se o cookie estiver presente
+    }
+
+    // Ao clicar no botão de aceitar
+    acceptBtn.onclick = () => {
+        document.cookie = "CookieAccepted=true; max-age=" + 60 * 60 * 24 * 30 + "; path=/"; // Cria um cookie que expira em 30 dias e é acessível em todas as páginas
+        cookieBox.classList.add("hide"); // Oculta o pop-up
+    };
+});
+
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
